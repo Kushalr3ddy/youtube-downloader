f820d5dd26ea7620db9d53f979ad8313c96c56d9
Kushalr3ddy <pikkikushal@gmail.com>
2020-09-07 22:34:44 +0530

    safe0

--- test.py
+++ test.py
@@ -2,32 +2,22 @@
 itags, progressive
 18 = 360p
 135 = 480p
-
+22=720p 
 '''
 '''from moviepy.editor import *
 video = VideoFileClip("1w.mp4")
 video.audio.write_audiofile("1w.mp3")'''
 
 #https://www.youtube.com/watch?v=a3HZ8S2H-GQ
+#4k
+#https://www.youtube.com/watch?v=OfIQW6s1-ew
+from pytube import YouTube
+link = 'https://www.youtube.com/watch?v=OfIQW6s1-ew'
+yt = YouTube(link)
 
-from tkinter import *
 
+for i in yt.streams.filter():
+    print(i)
+    print("\n")
 
-top = Tk()
-
-mb=  Menubutton ( top, text="condiments", relief=RAISED )
-mb.grid()
-mb.menu =  Menu ( mb, tearoff = 0 )
-mb["menu"] =  mb.menu
-
-mayoVar = IntVar()
-ketchVar = IntVar()
-
-mb.menu.add_checkbutton ( label="mayo",
-                          variable=mayoVar )
-mb.menu.add_checkbutton ( label="ketchup",
-                          variable=ketchVar )
-
-mb.pack()
-top.mainloop()
-
+#yt.streams.get_by_itag(160).download()
