399c90ec2e59a6edec58133dc58b3d884dad036b
Kushalr3ddy <pikkikushal@gmail.com>
2020-09-07 19:34:00 +0530

    safe0

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 '''
-itags
+itags, progressive
 18 = 360p
 135 = 480p
 
@@ -9,54 +9,25 @@ video = VideoFileClip("1w.mp4")
 video.audio.write_audiofile("1w.mp3")'''
 
 #https://www.youtube.com/watch?v=a3HZ8S2H-GQ
-'''
-import pytube
-
-link ="https://www.youtube.com/watch?v=a3HZ8S2H-GQ"
-
-yt = pytube.YouTube(link)
-
-for i in yt.streams.filter(file_extension='mp4'):
-   print(i)
-   print("\n")
-
-#yt.streams.get_by_itag('18').download()
-import os
-from moviepy.editor import *
-video = VideoFileClip(os.path.join("w.mp4"))
-video.audio.write_audiofile(os.path.join("videos","movie_sound.mp3"))
-'''
-
-
-#ffmpeg -i video.mp4 -vn audio.mp3
 
 from tkinter import *
 
-def sel():
-   selection = "You selected the option " + str(var.get())
-   label.config(text = selection)
 
-root = Tk()
-var = IntVar()
-R1 = Radiobutton(root, text="Option 1", variable=var, value=1,
-                  command=sel)
-R1.pack( anchor = W )
+top = Tk()
 
-R2 = Radiobutton(root, text="Option 2", variable=var, value=2,
-                  command=sel)
-R2.pack( anchor = W )
+mb=  Menubutton ( top, text="condiments", relief=RAISED )
+mb.grid()
+mb.menu =  Menu ( mb, tearoff = 0 )
+mb["menu"] =  mb.menu
 
-R3 = Radiobutton(root, text="Option 3", variable=var, value=3,
-                  command=sel)
-R3.pack( anchor = W)
+mayoVar = IntVar()
+ketchVar = IntVar()
 
-var1 = IntVar()
+mb.menu.add_checkbutton ( label="mayo",
+                          variable=mayoVar )
+mb.menu.add_checkbutton ( label="ketchup",
+                          variable=ketchVar )
 
-e = Radiobutton(root,text="x",variable=var1)
-y =Radiobutton(root,text="y",variable=var1)
-e.pack()
-y.pack()
+mb.pack()
+top.mainloop()
 
-label = Label(root)
-label.pack()
-root.mainloop()
